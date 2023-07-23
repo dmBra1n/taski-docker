574268aaa1b33d128d824be4dd5866438a2bd7e6
Vadim <103333041+dmBra1n@users.noreply.github.com>
2023-07-23 17:36:39 +0300

    fix CI/CD

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -3,12 +3,11 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.getenv('SECRET_KEY', default='TOKEN')
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+DEBUG = False
 
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['51.250.110.115', '127.0.0.1', 'localhost', 'studykittygram.hopto.org']
 
 
 # Application definition
